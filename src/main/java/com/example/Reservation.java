6312a2120e765d022bf5513f210cdc6617c1714e
wengefan <fwg1126@gmail.com>
2018-02-09 12:03:53 +0900

    Create Reservation.java

---
+++ src/main/java/com/example/Reservation.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.seasar.doma.Entity;
+import org.seasar.doma.GeneratedValue;
+import org.seasar.doma.GenerationType;
+import org.seasar.doma.Id;
+
+@Entity
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer id;
+    public String name;
+    public String details;
+    public Double amount;
+    public String expenseCategory;
+    public Boolean approved;
+}
